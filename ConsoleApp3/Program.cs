70023d2b74f9a4eca6d9304365d379dbb0db7f6d
gleborase <krasnopolsci@gmail.com>
2025-02-03 12:41:45 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{/// <summary>
+/// Задача 4. Составьте программу вычисления суммы 150 первых членов арифметической прогрессии, если a1= –200; d = 0,2.
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = -200;
+            double d = 0.2;
+            int n = 150;
+            double sum = n / 2.0 * (2 * a1 + (n - 1) * d);
+
+            Console.WriteLine($"Sum= {sum}");
+            Console.ReadKey();
+        }
+    }
+}
