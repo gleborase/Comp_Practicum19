0f7388a13add348a789d6d19c2da551a5e4d50b0
gleborase <krasnopolsci@gmail.com>
2025-02-03 13:11:28 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{/// <summary>
+/// Задача 5. Начав тренировки, лыжник в первый день пробежал 10 км. Каждый следующий день он увеличивал пробег на 10% от пробега предыдущего дня. Определить:
+///б) в какой день суммарный пробег за все дни превысит 100 км.
+/// </summary>
+    internal class Program
+    {
+        static void Main()
+        {
+            double distance = 10;
+            double totalDistance = 10;
+            int day = 1;
+
+            while (totalDistance <= 100)
+            {
+                day++;
+                distance *= 1.1;
+                totalDistance += distance;
+            }
+
+            Console.WriteLine($"Суммарный пробег превысит 100 км на {day}-й день.");
+            Console.Read();
+        }
+    }
+}
